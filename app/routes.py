b0fa413e50a078bc543b4ef887767e2c9547f90f
senjasd <senjasd@gmail.com>
2021-11-16 00:34:43 +0300

    laba3

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,22 @@
+# -*- coding: utf-8 -*-
+from flask import render_template
 from app import app
-@app.route('/')
 @app.route('/index')
+@app.route('/')
 def index():
-    return "Hello, World"
 
+    user = {'username': "student" , 'use': 'calculator'}
+    return render_template('index.html', title='Home', user=user)
+
+@app.route('/stat')
+def stat():
+    user = {'username': "SEM" , 'use': 'game'}
+    return '''<html>
+    <head>
+    <title> Home  - Microblog</title>
+    </head>
+    <body>
+    <h1>Hello,''' + user["username"] + '''!</h1>
+    <h2>come on , ''' + user['use'] + '''!</h2>
+    </body>
+    </html>'''
