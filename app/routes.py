809b1a5739e7cba59529658845db9d4850fb1493
senjasd <senjasd@gmail.com>
2021-11-16 02:08:21 +0300

    laba3.last_version

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,32 @@
 # -*- coding: utf-8 -*-
 from flask import render_template
 from app import app
-@app.route('/index')
-@app.route('/')
-def index():
 
-    user = {'username': "student" , 'use': 'calculator'}
-    return render_template('index2.html', title='Home', user=user)
+
+@app.route('/')
+def base():
+    return render_template('base.html', title='Home')
+
+
+@app.route('/index4')
+def index4():
+    user = {'username': "student", 'use': 'calculator'}
+    names_fraz = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        },
+        {
+            'author': {'username': 'Василий'},
+            'body': 'Привет!!'
+        }
+    ]
+    return render_template('index4.html', title='Home', user=user, names_fraz=names_fraz)
+
 
 @app.route('/stat')
 def stat():
